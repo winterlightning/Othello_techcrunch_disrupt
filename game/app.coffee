4e9b279bbf739da2541f8b02675ea06a48633156
winterlightning <raycornellwang@gmail.com>
2012-05-19 14:46:46 -0400

    board creation

--- game/app.coffee
+++ game/app.coffee
@@ -1,6 +1,49 @@
-#model for each Grid
+$ = jQuery
 
+#model for each Grid
+class Grid extends Spine.Model
+  @configure "Grid", "content"
 
 #controller for each Grid
+class GridItem extends Spine.Controller
+  
+  constructor: ->
+    super
+    @item.bind("update",  @render)
+    
+  render: =>
+    @replace( "<td>#{@item.content}</td>" )
+    @
+  
+  update_content: ->
+    console.log("update content")
 
 #App for the overall game
+class OthelloGame extends Spine.Controller
+  el: "#reversi_board"
+
+  constructor: ->
+    super
+    Grid.bind("update",  @evaluate)
+    
+    for i in ["1", "2", "3", "4", "5", "6", "7", "8"]
+      for h in ["1", "2", "3", "4", "5", "6", "7", "8"]
+        Grid.create id: i+h, content: "O"
+    
+    @addall()
+
+  addall: ->
+    console.log("add all")
+
+    for i in ["1", "2", "3", "4", "5", "6", "7", "8"]
+      tr = $("<tr></tr>")
+        
+      for h in ["1", "2", "3", "4", "5", "6", "7", "8"]
+        key = i+h
+        a = Grid.find(key)
+        
+        view = new GridItem(item: a)
+        tr.append(view.render().el)
+      
+      @el.append(tr)
+
--- game/app.js
+++ game/app.js
@@ -1,3 +1,80 @@
 (function() {
-
+  var $, Grid, GridItem, OthelloGame;
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  $ = jQuery;
+  Grid = (function() {
+    __extends(Grid, Spine.Model);
+    function Grid() {
+      Grid.__super__.constructor.apply(this, arguments);
+    }
+    Grid.configure("Grid", "content");
+    return Grid;
+  })();
+  GridItem = (function() {
+    __extends(GridItem, Spine.Controller);
+    function GridItem() {
+      this.render = __bind(this.render, this);      GridItem.__super__.constructor.apply(this, arguments);
+      this.item.bind("update", this.render);
+    }
+    GridItem.prototype.render = function() {
+      this.replace("<td>" + this.item.content + "</td>");
+      return this;
+    };
+    GridItem.prototype.update_content = function() {
+      return console.log("update content");
+    };
+    return GridItem;
+  })();
+  OthelloGame = (function() {
+    __extends(OthelloGame, Spine.Controller);
+    OthelloGame.prototype.el = "#reversi_board";
+    function OthelloGame() {
+      var h, i, _i, _j, _len, _len2, _ref, _ref2;
+      OthelloGame.__super__.constructor.apply(this, arguments);
+      Grid.bind("update", this.evaluate);
+      _ref = ["1", "2", "3", "4", "5", "6", "7", "8"];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        i = _ref[_i];
+        _ref2 = ["1", "2", "3", "4", "5", "6", "7", "8"];
+        for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+          h = _ref2[_j];
+          Grid.create({
+            id: i + h,
+            content: "O"
+          });
+        }
+      }
+      this.addall();
+    }
+    OthelloGame.prototype.addall = function() {
+      var a, h, i, key, tr, view, _i, _j, _len, _len2, _ref, _ref2, _results;
+      console.log("add all");
+      _ref = ["1", "2", "3", "4", "5", "6", "7", "8"];
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        i = _ref[_i];
+        tr = $("<tr></tr>");
+        _ref2 = ["1", "2", "3", "4", "5", "6", "7", "8"];
+        for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+          h = _ref2[_j];
+          key = i + h;
+          a = Grid.find(key);
+          view = new GridItem({
+            item: a
+          });
+          tr.append(view.render().el);
+        }
+        _results.push(this.el.append(tr));
+      }
+      return _results;
+    };
+    return OthelloGame;
+  })();
 }).call(this);
