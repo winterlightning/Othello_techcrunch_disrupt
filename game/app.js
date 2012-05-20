7d2be16f183d496a333737325f22234d3776bf8a
winterlightning <raycornellwang@gmail.com>
2012-05-20 04:39:01 -0400

    a lot of fixes for player entry

--- game/app.js
+++ game/app.js
@@ -269,6 +269,7 @@
     });
   };
   apiReady = function(eventObj) {
+    var cur_player;
     if (eventObj.isApiReady) {
       console.log("API is ready");
       gapi.hangout.data.onStateChanged.add(function(eventObj) {
@@ -283,17 +284,30 @@
         if (state["x"] != null) {
           window.player = "O";
           cur_player = gapi.hangout.getParticipants()[0];
-          $("o_text").html(cur_player.displayName);
+          $("o_text").html(cur_player["person"].displayName);
           return $("x_text").html(state["x"]);
         } else {
           window.player = "X";
           cur_player = gapi.hangout.getParticipants()[0];
           gapi.hangout.data.submitDelta({
-            'x': cur_player.displayName
+            'x': cur_player["person"].displayName
           });
           return $("x_text").html(cur_player.displayName);
         }
       });
+      if (state["x"] != null) {
+        window.player = "O";
+        cur_player = gapi.hangout.getParticipants()[0];
+        $("o_text").html(cur_player.displayName);
+        $("x_text").html(state["x"]);
+      } else {
+        window.player = "X";
+        cur_player = gapi.hangout.getParticipants()[0];
+        gapi.hangout.data.submitDelta({
+          'x': cur_player.displayName
+        });
+        $("x_text").html(cur_player.displayName);
+      }
       console.log(gapi.hangout.getParticipants());
       return gapi.hangout.onApiReady.remove(apiReady);
     }
